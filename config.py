ea8b52829647fef218d625621d6fda5bf86cc2f6
Matthew <rivermatt@pm.me>
2022-10-14 18:36:44 -0600

    Added monad controls and changed qtileconfig to config.py

--- config.py
+++ config.py
@@ -55,6 +55,7 @@ keys = [
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    Key(["mod1"], "Tab", lazy.layout.next(), desc="Move window focus to other window"),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
@@ -63,10 +64,22 @@ keys = [
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod, "control"], "h",
+    lazy.layout.grow_left(),
+    lazy.layout.shrink_main().when(layout="monadtall"),
+    desc="Grow window to the left"),
+    Key([mod, "control"], "l",
+    lazy.layout.grow_right(),
+    lazy.layout.grow_main().when(layout="monadtall"),
+    desc="Grow window to the right"),
+    Key([mod, "control"], "j",
+    lazy.layout.grow_down(),
+    lazy.layout.grow_main().when(layout="monadwide"),
+    desc="Grow window down"),
+    Key([mod, "control"], "k",
+    lazy.layout.grow_up(),
+    lazy.layout.shrink_main().when(layout="monadwide"),
+    desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
@@ -81,12 +94,16 @@ keys = [
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod, "shift"], "Tab", lazy.prev_layout(), desc="Toggle between layouts"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
     Key([mod], "d", lazy.spawn("dmenu_run"), desc="Spawn dmenu_run"),
-    Key(["control", "mod1"], "l", lazy.spawn("slock"), desc="Locks the screen"),
+    Key(["control", "mod1"], "space", lazy.spawn("slock"), desc="Locks the screen"),
+    Key(["control", "mod1"], "p", lazy.spawn("flameshot gui"), desc="Takes a screenshot"),
+    Key([mod], "f", lazy.spawn("librewolf"), desc="Launch LibreWolf"),
+    Key([mod, "shift"], "f", lazy.spawn("librewolf --private-window"), desc="Launch LibreWolf private"),
     
     # Function key binds
     Key([], "XF86Search", lazy.spawn("dmenu_run"), desc="Spawn dmenu_run"),
@@ -131,6 +148,8 @@ for i in groups:
     )
 
 layouts = [
+    layout.MonadTall(ratio=0.6, border_focus="#aaaa00", border_width=2, margin=7, insert_position=1),
+    layout.MonadWide(ratio=0.7, border_focus="#aaaa00", border_width=2, margin=7, insert_position=1),
     #layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=2,
     layout.Columns(border_focus="#aaaa00", border_width=2, margin=7, insert_position=1),
     layout.Max(),
@@ -138,8 +157,6 @@ layouts = [
     #layout.Stack(num_stacks=2),
     #layout.Bsp(),
     #layout.Matrix(),
-    layout.MonadTall(border_focus="#aaaa00", border_width=2, margin=3),
-    layout.MonadWide(border_focus="#aaaa00", border_width=2, margin=3),
     #layout.RatioTile(),
     #layout.Tile(),
     #layout.TreeTab(),
@@ -156,7 +173,7 @@ extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-        wallpaper="~/Pictures/KDE-Plasma-Dark-710-HD-WL.jpg",
+        wallpaper="~/Pictures/wallpaper/KDE-Plasma-Dark-822-HD-WL.jpg",
         wallpaper_mode="fill",
         top=bar.Bar(
             [
@@ -174,7 +191,7 @@ screens = [
                 #widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
                 widget.Systray(),
                 widget.Sep(),
-                widget.Battery(foreground="bbbb00"),
+                widget.Battery(foreground="bbbb00", update_interval=30),
                 widget.Sep(),
                 widget.TextBox("Vol:"),
                 widget.PulseVolume(update_interval=0.2, limit_max_volume=True),
