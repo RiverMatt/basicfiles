252fba20248baa9609b212399837552cf7c9c314
RiverMatt <47513427+RiverMatt@users.noreply.github.com>
2024-09-25 23:59:17 -0600

    Add files via upload

---
+++ record_audio.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if ["$#" -ne 2]; then
+	echo "Usage: $0 <duration in seconds> <output filename>.wav. Exiting..."
+	exit 1
+fi
+
+DURATION=$1
+OUTPUT_NAME=$2
+
+if ! [[ "$DURATION" =~ ^[0-9]+$ ]]; then
+	echo "Error: Duration must be a positive number. Exiting..."
+	exit 1
+fi
+
+echo "Available audio sinks:"
+pactl list sources short
+echo "Please enter the desired sink ID:"
+read SINK
+
+
+echo "Recording for $DURATION seconds from sink $SINK to $OUTPUT_NAME"
+
+sudo ydotool key 164:1 164:0
+
+timeout "$DURATION" pw-record --target="$SINK" "$OUTPUT_NAME"
+
+sudo ydotool key 164:1 164:0
+
+
+echo "Recording complete. Audio saved to $OUTPUT_NAME."
